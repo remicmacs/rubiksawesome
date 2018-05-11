bb180e303e4cecb7a0289ec2cb24f90a28dc32aa
hexliath <contact@hexliath.fr>
2018-05-11 21:11:15 +0200

    impl. method fridrich

--- src/controller/f2l.c
+++ src/controller/f2l.c
@@ -6,17 +6,43 @@ return self;
 }
 
 void searchWhiteEdge(cube self){
+<<<<<<< HEAD
 for (int faceIndex = F ; faceIndex < D+1 ; faceIndex++){
 		for (int index = 0 ; index < 3 ; index++){
 			for (int jindex = 0 ; jindex < 3 ; jindex++){
 				if(isEdge()){
 				printf("%c",self.cube[faceIndex][index][jindex]);
+=======
+
+for (int faceIndex = F ; faceIndex < D+1 ; faceIndex++){
+		for (int index = 0 ; index < 3 ; index++){
+			for (int jindex = 0 ; jindex < 3 ; jindex++){
+				int element[2] = {index,jindex};
+				if(isEdge(element)){
+				if (self.cube[faceIndex][index][jindex] == 'w'){
+				printf("this is a white edge");
+				};
+>>>>>>> impl. method fridrich
 				}
 			}
 		}
 	}
 }
 
+<<<<<<< HEAD
 bool isEdge(){
 	return true;
+=======
+bool isEdge(int* element){
+	int edges[4][2] = {
+		{0, 1}, {1, 0},
+	       	{1, 2}, {2, 1}
+	};
+	for (int i=0;i < 4;i++){
+	if(edges[i][0] == element[0] && edges[i][1] == element[1]){
+		return true;
+	}	
+	}
+	return false;
+>>>>>>> impl. method fridrich
 }
--- src/controller/f2l.h
+++ src/controller/f2l.h
@@ -2,7 +2,20 @@
 #include <string.h>
 #include "../model/cube.h"
 #include <stdbool.h>
+
+
+/**
+ * Do the white cross, first step to solve the Rubik’s Cube
+ */
 cube whiteCross(cube rubikscube);
+
+/*
+ * To assemble the white cross, we need to find all white/another color element.
+ */
 void searchWhiteEdge(cube rubikscube);
 
-bool isEdge();
+/**
+ * Test if the element is an edge
+ */
+bool isEdge(int* element);
+
