37d6203d8d481079b9b3a8624a99252664f24c8e
remicmacs <rembourgeon@gmail.com>
2018-05-18 15:51:31 +0200

    Implemented parseCommand() and executeBulkCommand() functions. Minor modification of cube. Added utils.h with error checked malloc and realloc

---
+++ src/controller/commandParser.h
@@ -0,0 +1,38 @@
+#ifndef COMMAND_PARSER_H
+#define COMMAND_PARSER_H
+#include "../model/cube.h"
+#include "utils.h"
+#include "errorController.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Parse and converts a string of commands in an array of moves
+ *
+ * Given a string of well-formed commands, returns an array of move commands.
+ * This array of move can be fed to executeCommand with the cube to modify.
+ *
+ * @param str String of whitespace separated well-formed commands.
+ *  a command is well-formed if it complies to the conditions defined in the
+ *  [cube.h](../model/cube.h) file @see move
+ *
+ * @returns : a pointer to a move array. NULL if an incorrect command has been
+ * found in the str
+ */
+move * commandParser(char * str);
+
+/**
+ * Apply a chain of commands on a given cube.
+ * This function takes a list of commands as an array of move. it applies the
+ * moves to the cube pointed to by the cube pointer fed as parameter.
+ *
+ * @params aCube pointer to the cube to modify
+ * @params moves : array of moves terminated by -1
+ * @see move
+ *
+ * @returns pointer to cube if it is a correct list of moves and the cube exists
+ */
+cube * executeBulkCommand(cube * aCube, move * moves);
+
+#endif
---
+++ src/controller/utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <stdlib.h>
+#include "errorController.h"
+
+/**
+ * Error checked malloc
+ */
+void * ec_malloc(size_t size);
+
+/**
+ * Error checked realloc
+ */
+void * ec_realloc(void * ptr, size_t size);
+#endif
